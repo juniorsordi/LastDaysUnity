34914d0024fec2235365640a7b15ec70feb97c2b
dilson <juniorsordi@gmail.com>
2016-10-31 17:10:15 -0200

    2016.10.30.1

---
+++ Assets/Game/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VitalzeroGames {
+
+	public class PlayerManager : MonoBehaviour {
+
+		public string playerName;
+		public int playerLevel;
+		public int playerExperience;
+		public float playerHunger;
+		public float playerThrist;
+		public float playerStamina;
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+
+}
---
+++ Assets/Game/Scripts/ResourceManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VitalzeroGames {
+
+	public class ResourceManager : MonoBehaviour {
+
+		public enum ResourceTypes { Wood, Plastic, Metal, Fuel, Water, Food };
+
+		public int woodResource;
+		public int plasticResource;
+		public int metalResource;
+		public int fuelResource;
+		public int waterResource;
+		public int foodResource;
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+
+}
