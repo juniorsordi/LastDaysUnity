013ad9dcf6e58610464387e32d0a526120355206
dilson <juniorsordi@gmail.com>
2016-11-03 17:35:27 -0200

    20161103.2

---
+++ Assets/Game/Scripts/PlayerBuilding.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VitalzeroGames {
+
+	public class PlayerBuilding : MonoBehaviour {
+
+		public GameObject prefab;
+		public float someDistance;
+
+		private GameObject player;
+
+		// Use this for initialization
+		void Start () {
+			player = GameObject.FindWithTag("Player");
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void Build() {
+			Vector3 pos = transform.position + transform.forward * someDistance;
+			pos.y = Terrain.activeTerrain.SampleHeight(pos);
+			prefab.transform.position = pos + Vector3.up * 1;
+			prefab.transform.rotation = Quaternion.LookRotation(transform.forward);
+			GameObject obj = (GameObject) Instantiate(prefab, pos, player.transform.rotation);
+		}
+	}
+
+}
